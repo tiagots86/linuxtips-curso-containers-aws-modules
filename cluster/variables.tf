a8efd0050ab2d3a8eb0ced23fc7536e15b3c747b
Tiago Teixeira Santos <tiagots86@hotmail.com>
2024-12-01 19:17:40 -0300

    cluster

---
+++ cluster/variables.tf
@@ -0,0 +1,34 @@
+##### General Configs #####
+
+variable "project_name" {
+  description = "O nome do projeto, usado para nomear recursos no escopo deste Terraform."
+  type        = string
+}
+
+variable "region" {
+  description = "A região da AWS onde os recursos serão criados."
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID da VPC"
+  type        = string
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+#### ECS General ######
+
+variable "capacity_providers" {
+  type        = list(any)
+  description = "A lista dos capacity providers que serão permitidos no cluster fargate"
+  default = [
+    "FARGATE", "FARGATE_SPOT"
+  ]
+}
